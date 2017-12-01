2e60ffcfc59af461c3d48d158e0d15b433f90754
Philip <philip.balbas@gmail.com>
2017-12-01 16:45:58 +0800

    fix photo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,48 +4,48 @@ const Sidebar = props => (
   <div className="sidebar bg-white">
     <div className="sidebar-divider">
       <h6>HOME</h6>
-      <a className="sidebar-button">
+      <p className="sidebar-button">
         <i className="fa fa-newspaper-o" aria-hidden="true" />Dashboard
-      </a>
+      </p>
     </div>
     <div>
       <h6>USERS</h6>
-      <a className="sidebar-button">
+      <p className="sidebar-button">
         <i className="fa fa-users" aria-hidden="true" />Customers
-      </a>
-      <a className="sidebar-button">
+      </p>
+      <p className="sidebar-button">
         <i className="fa fa-users" aria-hidden="true" />Signups
-      </a>
-      <a className="sidebar-button">
+      </p>
+      <p className="sidebar-button">
         <i className="fa fa-area-chart" aria-hidden="true" />Providers
-      </a>
-      <a className="sidebar-button">
+      </p>
+      <p className="sidebar-button">
         <i className="fa fa-th-large" aria-hidden="true" />Admins
-      </a>
+      </p>
     </div>
     <div>
       <h6>TESTS</h6>
-      <a className="sidebar-button">
+      <p className="sidebar-button">
         <i className="fa fa-calendar" aria-hidden="true" />Bookings
-      </a>
-      <a className="sidebar-button">
+      </p>
+      <p className="sidebar-button">
         <i className="fa fa-users" aria-hidden="true" />Test Results
-      </a>
-      <a className="sidebar-button">
+      </p>
+      <p className="sidebar-button">
         <i className="fa fa-users" aria-hidden="true" />Packages
-      </a>
+      </p>
     </div>
     <div>
       <h6>Partners</h6>
-      <a className="sidebar-button">
+      <p className="sidebar-button">
         <i className="fa fa-building-o" aria-hidden="true" />Labs/Clinics
-      </a>
+      </p>
     </div>
     <div>
       <h6>Others</h6>
-      <a className="sidebar-button">
+      <p className="sidebar-button">
         <i className="fa fa-envelope" aria-hidden="true" />Newsletter
-      </a>
+      </p>
     </div>
   </div>
 )
